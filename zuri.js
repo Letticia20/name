9c0e187e6a9bbb12bb1bec01ec1fe1d3094644a9
Adimoha Letticia <adimohaletticia@gmail.com>
2022-06-24 13:18:54 +0100

    first commit

---
+++ zuri.js
@@ -0,0 +1,9 @@
+/*type="text/javascript"
+var a,b,c, name, height, country;
+a=parseInt(prompt(Name));
+b=parseInt(prompt(Height));
+c=parseInt(prompt(Country));
+*/
+alert("Name: \n Adimoha Letticia")
+    alert("Height: \n 1.65") 
+    alert("Country: \n Nigeria")
